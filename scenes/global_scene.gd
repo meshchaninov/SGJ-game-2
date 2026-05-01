770d5f9aa917a54bdbeedc45f0e3a41ffc6ee56f
Grigoriy Kustov <grishakustov@yandex.ru>
2026-05-01 14:00:00 +0700

    init commit 2

---
+++ scenes/global_scene.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+var current_blob_state = [1, 2, 3, 4, 5]
+var true_blob_state = [1, 1, 1, null, null]
+
+func set_true_blob_state(newState: Array) -> void:
+	true_blob_state = newState
+
+func set_current_blob_state(newState: Array) -> void:
+	true_blob_state = newState

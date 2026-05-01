e654a2490f7f6c326805537cf721b07d0954b2b2
Grigoriy Kustov <grishakustov@yandex.ru>
2026-05-01 14:33:22 +0700

    Editor placeholder added, basic ui placeholders added

--- scenes/global_scene.gd
+++ scenes/global_scene.gd
@@ -4,6 +4,10 @@ extends Node2D
 var current_blob_state = [1, 2, 3, 4, 5]
 var true_blob_state = [1, 1, 1, null, null]
 
+static const PARTS = {
+	'EYE_1': 
+}
+
 func set_true_blob_state(newState: Array) -> void:
 	true_blob_state = newState
 

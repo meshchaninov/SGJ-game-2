0aa8c1f6fd5cfcbd36053dde14a8c61c979ddf28
Grigoriy Kustov <grishakustov@yandex.ru>
2026-05-01 14:37:29 +0700

    Added parts enum

--- scenes/global_scene.gd
+++ scenes/global_scene.gd
@@ -4,8 +4,9 @@ extends Node2D
 var current_blob_state = [1, 2, 3, 4, 5]
 var true_blob_state = [1, 1, 1, null, null]
 
-static const PARTS = {
-	'EYE_1': 
+static var PARTS = {
+	'EYE_1': "res://assets/pics/blob/parts/eye/1.png",
+	'EYE_2': "res://assets/pics/blob/parts/eye/2.png"
 }
 
 func set_true_blob_state(newState: Array) -> void:

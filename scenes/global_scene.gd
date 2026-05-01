28178a6bda704d9ea2c8788f2b32497f4026a4e1
Grigoriy Kustov <grishakustov@yandex.ru>
2026-05-01 14:48:51 +0700

    Parts array created

--- scenes/global_scene.gd
+++ scenes/global_scene.gd
@@ -4,10 +4,14 @@ extends Node2D
 var current_blob_state = [1, 2, 3, 4, 5]
 var true_blob_state = [1, 1, 1, null, null]
 
-static var PARTS = {
+var max_parts = [2, 0, 0 ,0 ,0]
+
+var PARTS = {
 	'EYE_1': "res://assets/pics/blob/parts/eye/1.png",
 	'EYE_2': "res://assets/pics/blob/parts/eye/2.png"
 }
+
+var PARTS_1 = [PARTS['EYE_1'], PARTS['EYE_2']]
 
 func set_true_blob_state(newState: Array) -> void:
 	true_blob_state = newState

14a5ee9f0f2ae78b48f93434fdf74a512d2241fd
Grigoriy Kustov <grishakustov@yandex.ru>
2026-05-01 20:49:23 +0700

    conflict resolve

--- scenes/editor/editor_row.gd
+++ scenes/editor/editor_row.gd
@@ -1,34 +1,54 @@
 extends Node2D
 
+class_name  EditorRow
 #@export var max_index: int
 #@export var paths_array: Array[String]
 
 @export var part_index: int
 @export var sprite: Sprite2D
 
+signal click(is_left: bool)
+
 
 var paths_array: Array[String] 
 
-
 func _ready() -> void:
-	print(part_index)
-	print(GlobalStateScene.PARTS_PER_ROW)
 	paths_array = GlobalStateScene.PARTS_PER_ROW[part_index]
-	sprite.texture = getTexture(paths_array[0])
+	update_texture()
 
-
+func update_texture():
+	var current = get_current_index()
+	sprite.texture = getTexture(paths_array[current])
+	
 func get_max_index() -> int: 
 	return GlobalStateScene.max_parts[part_index]
+	
+func get_current_index() -> int: 
+	return GlobalStateScene.current_blob_state[part_index]
 
 func getTexture(str: String) -> Texture2D:
 	var texture = load(str)
 	return texture
 
-func _on_button_right_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event.is_pressed():
-		print('right click')
+func _on_click(is_left: bool) -> void:
+	var current = get_current_index()
+	var max = get_max_index()
+	var next_index: int
+	var prev_index: int
 
-
-func _on_button_left_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event.is_pressed():
-		print('left click')
+	if(current == max):
+		next_index = 0
+	else:
+		next_index = current + 1	
+		
+	if(current == 0):
+		prev_index = max
+	else:
+		prev_index = current - 1
+	
+	if(is_left):
+		GlobalScene.set_current_blob_state_part(prev_index, part_index)
+	else:
+		GlobalScene.set_current_blob_state_part(next_index, part_index)
+	
+	update_texture()
--- scenes/global_scene.gd
+++ scenes/global_scene.gd
@@ -2,12 +2,12 @@ extends Node2D
 class_name GlobalScene
 
 
-var current_blob_state = [1, 2, 3, 4, 5]
-var true_blob_state = [1, 1, 1, null, null]
+static var current_blob_state = [0, 0, 0, 0, 0]
+static var true_blob_state = [1, 1, 1, null, null]
 
 # Тут показывается элемент массива до которого мы имеем доступ в части
 # если -1 то часть недоступна
-var max_parts = [1, -1, -1 ,-1 ,-1]
+static var max_parts = [1, -1, -1 ,-1 ,-1]
 
 # тут прост для удобство адреса всех пикч
 static var PARTS = {
@@ -22,8 +22,12 @@ static var PARTS_PER_ROW = {
 	0: PARTS_1
 }
 
-func set_true_blob_state(newState: Array) -> void:
+static func set_true_blob_state(newState: Array) -> void:
 	true_blob_state = newState
 
-func set_current_blob_state(newState: Array) -> void:
+static func set_current_blob_state_part(value: int, part_index: int) -> void:
+	current_blob_state[part_index] =  value
+	
+	
+static func set_current_blob_state(newState: Array) -> void:
 	true_blob_state = newState

387a77c160326097a16d153f4d0583ccda269d81
Nikita Meshchaninov <nmm33342@gmail.com>
2026-05-02 18:55:21 +0700

    music add

--- scenes/cameraView/Menu.gd
+++ scenes/cameraView/Menu.gd
@@ -1,8 +1,10 @@
 extends Node
 
 @onready var curtain: ColorRect = $Curtain/ColorRect
+@onready var menu_music: AudioStreamPlayer = $MenuMusic
+@onready var game_music: AudioStreamPlayer = $GameMusic
 
-var is_closed := false
+var is_closed := true
 var is_animating := false
 var tween: Tween
 
@@ -12,6 +14,10 @@ var shown_y := 0.0
 func _ready() -> void:
 	hidden_y = curtain.position.y
 	shown_y = 0.0
+	if menu_music and game_music:
+		menu_music.stream = preload("res://assets/audio/menu.mp3")
+		game_music.stream = preload("res://assets/audio/game.mp3")
+		game_music.play()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -32,18 +38,26 @@ func toggle_curtain() -> void:
 	tween.finished.connect(_on_tween_finished)
 
 	if is_closed:
-		tween.tween_property(curtain, "position:y", hidden_y, 0.32) \
-			.set_trans(Tween.TRANS_CUBIC) \
-			.set_ease(Tween.EASE_IN)
-		is_closed = false
-	else:
-		tween.tween_property(curtain, "position:y", 0.0, 0.36) \
+		tween.tween_property(curtain, "position:y", shown_y, 0.36) \
 			.set_trans(Tween.TRANS_CUBIC) \
 			.set_ease(Tween.EASE_OUT)
 		tween.tween_property(curtain, "position:y", shown_y, 0.12) \
 			.set_trans(Tween.TRANS_BACK) \
 			.set_ease(Tween.EASE_OUT)
+		is_closed = false
+	else:
+		tween.tween_property(curtain, "position:y", hidden_y, 0.32) \
+			.set_trans(Tween.TRANS_CUBIC) \
+			.set_ease(Tween.EASE_IN)
 		is_closed = true
 
 func _on_tween_finished() -> void:
 	is_animating = false
+	if not menu_music or not game_music:
+		return
+	if is_closed:
+		menu_music.play()
+		game_music.stop()
+	else:
+		game_music.play()
+		menu_music.stop()
--- scenes/cameraView/camera_view.gd
+++ scenes/cameraView/camera_view.gd
@@ -4,21 +4,28 @@ extends Node2D
 @onready var progress_bar: Control = $ProgressBar
 @onready var attempt: Control = $Attempt
 @onready var check: Control = $Check
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	clue.toggle_pressed.connect(_on_clue_toggle_pressed)
 	check.check_pressed.connect(_on_check_pressed)
 
 func _on_clue_toggle_pressed() -> void:
-	var percent = GlobalStateScene.checkWinPercent()
-	progress_bar.set_percent(percent)
-
-	GlobalStateScene.lives -= 1
-	attempt.change_lives(GlobalStateScene.lives)
+	_handle_check()
 
 func _on_check_pressed() -> void:
+	_handle_check()
+
+func _handle_check() -> void:
 	var percent = GlobalStateScene.checkWinPercent()
 	progress_bar.set_percent(percent)
 
 	GlobalStateScene.lives -= 1
 	attempt.change_lives(GlobalStateScene.lives)
+
+	if percent < 66:
+		sprite.play_temp_animation("bad")
+	elif percent < 80:
+		sprite.play_temp_animation("normal")
+	elif percent == 100:
+		sprite.play_temp_animation("good")
